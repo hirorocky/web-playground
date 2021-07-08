0c9fc942ce3c26f957501d51b48497cb0bd54c1e
hirorocky <hirorocky@users.noreply.github.com>
2021-07-08 14:04:58 +0000

    deploy: 766cdf6fc82e8916469cec20864a9a92e35318cd

--- _nuxt/static/1625666175/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fweb-playground\u002F_nuxt\u002Fstatic\u002F1625666175",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fweb-playground\u002F",assetsPath:"\u002Fweb-playground\u002F_nuxt\u002F",cdnURL:a}}}}(null));
